5eeb318f0fff801cae8a804280813a8bc3e29039
jdowni000 <jdownie@redhat.com>
2022-11-04 08:21:41 -0500

    Black style changes and autoflake removal of unused imports

--- smallfile_plugin.py
+++ smallfile_plugin.py
@@ -12,34 +12,48 @@ import shutil
 import csv
 from arcaflow_plugin_sdk import plugin
 
-from smallfile_schema import WorkloadParams, WorkloadResults, WorkloadError, smallfile_schema, output_params_schema, output_results_schema, output_rsptimes_schema
+from smallfile_schema import (
+    WorkloadParams,
+    WorkloadResults,
+    WorkloadError,
+    smallfile_schema,
+    output_params_schema,
+    output_results_schema,
+    output_rsptimes_schema,
+)
 
 
-# The following is a decorator (starting with @). We add this in front of our function to define the metadata for our step.
+# The following is a decorator (starting with @). We add this in
+# front of our function to define the metadata for our step.
 @plugin.step(
     id="workload",
     name="smallfile workload",
     description="Run the smallfile workload with the given parameters",
     outputs={"success": WorkloadResults, "error": WorkloadError},
 )
-def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[WorkloadResults, WorkloadError]]:
+def smallfile_run(
+    params: WorkloadParams,
+) -> typing.Tuple[str, typing.Union[WorkloadResults, WorkloadError]]:
     """
-    The function  is the implementation for the step. It needs the decorator above to make it into a  step. The type
-    hints for the params are required.
+    The function  is the implementation for the step.
+    It needs the decorator above to make it into a  step.
+    The type hints for the params are required.
 
     :param params:
 
-    :return: the string identifying which output it is, as well the output structure
+    :return: the string identifying which output it is,
+    as well the output structure
     """
 
     smallfile_dir = "/plugin/smallfile"
     smallfile_yaml_file = tempfile.mkstemp()
     smallfile_out_file = tempfile.mkstemp()
 
-    # Copy all parameters from SmallfileParams directly for the smallfile CLI to use via YAML
+    # Copy all parameters from SmallfileParams directly
+    # for the smallfile CLI to use via YAML
     print("==>> Importing workload parameters ...")
     smallfile_params = smallfile_schema.serialize(params.smallfile_params)
-    with open(smallfile_yaml_file[1], 'w') as file:
+    with open(smallfile_yaml_file[1], "w") as file:
         file.write(yaml.dump(smallfile_params))
 
     smallfile_cmd = [
@@ -55,13 +69,23 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
     # Run smallfile workload
     print("==>> Running smallfile workload ...")
     try:
-        print(subprocess.check_output(smallfile_cmd,
-              cwd=smallfile_dir, text=True, stderr=subprocess.STDOUT))
+        print(
+            subprocess.check_output(
+                smallfile_cmd,
+                cwd=smallfile_dir,
+                text=True,
+                stderr=subprocess.STDOUT,
+            )
+        )
     except subprocess.CalledProcessError as error:
         temp_cleanup(smallfile_yaml_file, smallfile_dir)
-        return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0], error.returncode, error.output))
+        return "error", WorkloadError(
+            "{} failed with return code {}:\n{}".format(
+                error.cmd[0], error.returncode, error.output
+            )
+        )
 
-    with open(smallfile_out_file[1], 'r') as output:
+    with open(smallfile_out_file[1], "r") as output:
         smallfile_results = output.read()
 
     smallfile_json = json.loads(smallfile_results)
@@ -81,11 +105,21 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
     ]
 
     try:
-        print(subprocess.check_output(smallfile_process_cmd,
-              cwd=smallfile_dir, text=True, stderr=subprocess.STDOUT))
+        print(
+            subprocess.check_output(
+                smallfile_process_cmd,
+                cwd=smallfile_dir,
+                text=True,
+                stderr=subprocess.STDOUT,
+            )
+        )
     except subprocess.CalledProcessError as error:
         temp_cleanup(smallfile_out_file, smallfile_dir)
-        return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0], error.returncode, error.output))
+        return "error", WorkloadError(
+            "{} failed with return code {}:\n{}".format(
+                error.cmd[0], error.returncode, error.output
+            )
+        )
 
     rsptimes_schema_to_results_map = {
         "host_thread": "host:thread",
@@ -97,15 +131,21 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
         "pctile50": " 50%ile",
         "pctile90": " 90%ile",
         "pctile95": " 95%ile",
-        "pctile99": " 99%ile"
+        "pctile99": " 99%ile",
     }
 
     smallfile_rsptimes = []
     try:
-        with open("{}/stats-rsptimes.csv".format(rsptime_dir), newline='') as csvfile:
+        with open(
+            "{}/stats-rsptimes.csv".format(rsptime_dir), newline=""
+        ) as csvfile:
             rsptimes_csv = csv.DictReader(csvfile)
             for row in rsptimes_csv:
-                if not re.match("per-", row["host:thread"]) and not re.match("cluster-", row["host:thread"]) and not re.match("time-", row["host:thread"]):
+                if (
+                    not re.match("per-", row["host:thread"])
+                    and not re.match("cluster-", row["host:thread"])
+                    and not re.match("time-", row["host:thread"])
+                ):
                     schema_row = dict(rsptimes_schema_to_results_map)
                     for skey, svalue in schema_row.items():
                         for key, value in row.items():
@@ -115,7 +155,11 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
                     smallfile_rsptimes.append(schema_row)
     except IOError as error:
         temp_cleanup(smallfile_out_file, smallfile_dir)
-        return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0], error.returncode, error.output))
+        return "error", WorkloadError(
+            "{} failed with return code {}:\n{}".format(
+                error.cmd[0], error.returncode, error.output
+            )
+        )
 
     # Cleanup after run, if enabled
     if params.cleanup:
@@ -128,22 +172,36 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
             "--yaml-input-file",
             smallfile_cleanup_yaml[1],
         ]
-        with open(smallfile_cleanup_yaml[1], 'w') as file:
+        with open(smallfile_cleanup_yaml[1], "w") as file:
             file.write(yaml.dump(cleanup_yaml))
         try:
-            print(subprocess.check_output(smallfile_cleanup_cmd,
-                  cwd=smallfile_dir, text=True, stderr=subprocess.STDOUT))
+            print(
+                subprocess.check_output(
+                    smallfile_cleanup_cmd,
+                    cwd=smallfile_dir,
+                    text=True,
+                    stderr=subprocess.STDOUT,
+                )
+            )
         except subprocess.CalledProcessError as error:
             temp_cleanup(smallfile_yaml_file, smallfile_dir)
             temp_cleanup(smallfile_cleanup_yaml)
-            return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0], error.returncode, error.output))
+            return "error", WorkloadError(
+                "{} failed with return code {}:\n{}".format(
+                    error.cmd[0], error.returncode, error.output
+                )
+            )
         temp_cleanup(smallfile_cleanup_yaml)
 
     temp_cleanup(smallfile_yaml_file, smallfile_dir)
     temp_cleanup(smallfile_out_file)
 
     print("==>> Workload run complete!")
-    return "success", WorkloadResults(output_params_schema.unserialize(smallfile_json["params"]), output_results_schema.unserialize(smallfile_json["results"]), output_rsptimes_schema.unserialize(smallfile_rsptimes))
+    return "success", WorkloadResults(
+        output_params_schema.unserialize(smallfile_json["params"]),
+        output_results_schema.unserialize(smallfile_json["results"]),
+        output_rsptimes_schema.unserialize(smallfile_rsptimes),
+    )
 
 
 def temp_cleanup(file=[], directory=""):
@@ -157,7 +215,11 @@ def temp_cleanup(file=[], directory=""):
 
 
 if __name__ == "__main__":
-    sys.exit(plugin.run(plugin.build_schema(
-        # List your step functions here:
-        smallfile_run,
-    )))
+    sys.exit(
+        plugin.run(
+            plugin.build_schema(
+                # List your step functions here:
+                smallfile_run,
+            )
+        )
+    )
